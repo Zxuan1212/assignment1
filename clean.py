a0ae236cbc16d04c262668664aaf0baf5f8e7363
Xuan <23401109@life.hkbu.edu.hk>
2024-02-05 13:22:58 +0800

    data cleaning script

--- clean.py
+++ clean.py
@@ -1,28 +1,14 @@
 import pandas as pd
+import pickle
 
+df1 = pd.read_csv('respondent_contact.csv')
+df2 = pd.read_csv('respondent_other.csv')
+merged_df = pd.merge(df1, df2, left_on='respondent_id', right_on='id')
+merged_df.drop('id', axis=1, inplace=True)
 
-def merge_files(input1, input2):
-    df1 = pd.read_csv(input1)
-    df2 = pd.read_csv(input2)
-    merged_df = pd.merge(df1, df2, left_on='respondent_id', right_on='id')
-    merged_df.drop('id', axis=1, inplace=True)
-    return merged_df
+cleaned_df = merged_df.dropna()
 
+cleaned_df2 = cleaned_df [~cleaned_df ['job'].str.contains('insurance|Insurance')]
 
-def drop_missing_rows(df):
-    cleaned_df = df.dropna()
-    return cleaned_df
-
-
-def drop_rows_with_insurance(df):
-    cleaned_df2 = df[~df['job'].str.contains('insurance|Insurance')]
-    return cleaned_df2
-
-
-def save_cleaned_data(df, output):
-    df.to_csv(output, index=False)
-
-merged_df = merge_files('respondent_contact.csv', 'respondent_other.csv')
-cleaned_df = drop_missing_rows(merged_df)
-cleaned_df2 = drop_rows_with_insurance(cleaned_df)
-print(cleaned_df2)
+with open('dataclean.pkl', 'wb') as file:
+    pickle.dump(cleaned_df2, file)
